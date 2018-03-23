6cd36fb0a0efad93d2a39b72a9ff9e1e0107e0c9
William Fisher <ashton0312@gmail.com>
2018-03-23 13:37:38 -0400

    Created Todo class and filled table with dummy data
    Created todo class and tested functionality by creating two dummy Todos
    in TodoTableViewController

---
+++ Simple Todo/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  Simple Todo
+//
+//  Created by William Fisher on 3/23/18.
+//  Copyright © 2018 William Fisher. All rights reserved.
+//
+
+import UIKit
+
+class Todo {
+    var name = ""
+    var isImportant = false
+}
---
+++ Simple Todo/TodoTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TodoTableViewController.swift
+//  Simple Todo
+//
+//  Created by William Fisher on 3/23/18.
+//  Copyright © 2018 William Fisher. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    var todos : [Todo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        todos = createTodos()
+    }
+    
+    func createTodos() -> [Todo] {
+        
+        let homework = Todo()
+        homework.name = "Homework"
+        homework.isImportant = true
+        
+        let dog = Todo()
+        dog.name = "Walk the dog"
+        dog.isImportant = false
+        
+        return [homework,dog]
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return todos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let todo = todos[indexPath.row]
+
+        cell.textLabel?.text = todo.name
+
+        return cell
+    }
+
+}
