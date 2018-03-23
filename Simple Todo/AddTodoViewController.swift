94cb3324e880cc469b0e4cec78605a83d9120c5f
William Fisher <ashton0312@gmail.com>
2018-03-23 15:22:57 -0400

    Created AddTodoViewController

---
+++ Simple Todo/AddTodoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddTodoViewController.swift
+//  Simple Todo
+//
+//  Created by William Fisher on 3/23/18.
+//  Copyright © 2018 William Fisher. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+    
+
+}
